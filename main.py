235d5ff1ab334df48cc3cb230b634c3b2ce5d6d7
NSpector <nspectorit@gmail.com>
2024-02-07 18:53:00 -0800

    V1.0 Discord Bot Notifications Receiver Retrieves the price notifications from the BTS Discord and then relays the price to trading bots specified.
    Squashed commit of the following:
    
    commit 7301ceccc1e8ad645f19e1ae8e0b5246edcd09cc
    Merge: a4a96ce a5c8774
    Author: NSpector <nspectorit@gmail.com>
    Date:   Wed Feb 7 18:27:36 2024 -0800
    
        Merge branch 'test-driver' into multiple-trading-bots
    
    commit a4a96ce1c90fc07c761598bd3516ecbc024b435d
    Author: NSpector <nspectorit@gmail.com>
    Date:   Wed Feb 7 18:27:06 2024 -0800
    
        Environment variable for trading bot api urls will be split by comma into a list. The list will be iterated and a price notification will be sent to each url
    
    commit e595a656bafdee988fa8172480da838543c40c6d
    Author: NSpector <nspectorit@gmail.com>
    Date:   Tue Jan 30 18:55:09 2024 -0800
    
        Made the trading bot api url an environment variable
    
    commit a5c8774c58a2a7c347fb501b05d6040fdaed9832
    Author: NSpector <nspectorit@gmail.com>
    Date:   Mon Jan 22 21:21:44 2024 -0800
    
        Added a python test file that will send a message into a specific channel and will imitate the price notification that the bts bot gives
    
    commit abb7c34896718eb2a85c0903f5915f8873e34b67
    Author: NSpector <nspectorit@gmail.com>
    Date:   Tue Jul 18 17:16:24 2023 -0700
    
        Modified so that we extract the price from the 'embed' instead of the 'content'
    
    commit ff5a8c71c41380d4e75ed05f7194ee82a27304fd
    Author: NSpector <nspectorit@gmail.com>
    Date:   Tue Jul 18 15:19:09 2023 -0700
    
        added new logger message
    
    commit 88b75408552bade4090dff493144f73d6eadd4a2
    Author: NSpector <nspectorit@gmail.com>
    Date:   Mon Jul 17 19:18:31 2023 -0700
    
        Setup for heroku
    
    commit 173bc021daedd557b4a85fe805dbc8cabc7c709f
    Author: NSpector <nspectorit@gmail.com>
    Date:   Mon Jul 17 19:17:26 2023 -0700
    
        Simplified code so that everything is in main. It will get the price from discord and send it to the trading bot

--- main.py
+++ main.py
@@ -1,16 +1,136 @@
-# This is a sample Python script.
+import discord
+import os
+import re
+import logging
+import graypy
+import requests
+import time
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+LOGGER_HOST = str(os.environ['LOGGER_HOST'])
+LOGGER_PORT = int(os.environ['LOGGER_PORT'])
+DISCORD_NOTIFICATIONS_CHANNEL = str(os.environ['DISCORD_NOTIFICATIONS_CHANNEL'])
+DISCORD_NOTIFICATIONS_BOT = str(os.environ['DISCORD_NOTIFICATIONS_BOT'])
+DISCORD_NOTIFICATIONS_DISABLE_VERIFY_BOT = bool(int(os.environ['DISCORD_NOTIFICATIONS_DISABLE_VERIFY_BOT']))
+DISCORD_TOKEN = str(os.environ['DISCORD_TOKEN'])
+
+TRADING_BOT_API_URLS = str(os.environ['TRADING_BOT_API_URLS']).split(",")
+
+LOGGER = logging.getLogger('discord-logger')
+LOGGER.setLevel(logging.DEBUG)
+
+handler = graypy.GELFTCPHandler(LOGGER_HOST, LOGGER_PORT)
+LOGGER.addHandler(handler)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+FORMAT = '%(asctime)s - %(levelname)s - %(message)s\n'
+logging.basicConfig(format=FORMAT, datefmt='%m/%d/%Y %H:%M:%S')
+# logging.disable(logging.DEBUG)
+
+def get_price_from_message(message):
+    try:
+        LOGGER.debug("Entering get_price_from_message()\n"
+                     f"discord_message: {message}\n")
+
+        price = re.findall(r"[-+]?(?:\d*\.*\d+)", f"{message}")
+        if len(price) > 0:
+            LOGGER.info(f"The price was retrieved from the message. price: {price[0]}")
+
+            return float(price[0])
+
+    except Exception as ex:
+        message = f"Error in get_price_from_message(): {ex}"
+        raise Exception(message)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#  Will check if the message came from the appropriate channel and from the bot.
+#  If it
+def verify_message(message):
+    try:
+        LOGGER.debug("Entering verify_message()\n"
+                           f"discord_message: {str(message.content)}\n"
+                           f"discord_embed: {str(message.embeds[0].description) if len(message.embeds) else None}\n"
+                           f"discord_author: {str(message.author)}\n"
+                           f"discord_channel: {str(message.channel)}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        channel = str(message.channel)
+        author = str(message.author)
+
+        #  Check if the message came from the notifications channel
+        if channel == DISCORD_NOTIFICATIONS_CHANNEL and (
+                author == DISCORD_NOTIFICATIONS_BOT or DISCORD_NOTIFICATIONS_DISABLE_VERIFY_BOT is True):
+
+            #  Check if the bot is giving a valid trade notification. The bot
+            #  uses 'embeds' to put the price notification into the chat.
+            if len(message.embeds) > 0:
+                content = message.embeds[0].description
+                if "NDX" in content:
+                    return content
+        return None
+
+    except Exception as ex:
+        message = f"Error in verify_message(): {ex}"
+        raise Exception(message)
+
+#  Sends a price notification to the bot's api endpoint with the specified base url
+def send_price_notification(url, price):
+    #  Send price to Trading Bot
+    url = f'{url}/notify?price={price}'
+    response = requests.post(url)
+
+    if response.status_code == 200:
+        LOGGER.info(f"Successfully sent price to trading bot!\n"
+                    f"trading bot api url: {url}")
+    else:
+        raise Exception(f"Could not send price to the trading bot!\n"
+                        f"trading bot api url: {url}")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        try:
+
+            LOGGER.debug(f"Entering on_message()\n"
+                        f"content: {message.content}\n"
+                        f"discord_embed: {str(message.embeds[0].description) if len(message.embeds) else None}\n"
+                        f"author: {message.author}\n"
+                        f"channel: {message.channel}")
+
+            #  Check if the message is a notification from the bot
+            message = verify_message(message)
+            if message:
+                LOGGER.info(f"A price notification was received from the bot.")
+                price = get_price_from_message(message)
+
+                #  Send a price notification to each trading bot that is setup
+                for url in TRADING_BOT_API_URLS:
+                    try:
+                        send_price_notification(url, price)
+                    except Exception as ex:
+                        LOGGER.error(f"Unable to send price notification to trading bot!\n"
+                                     f"trading bot api url: {url}\n"
+                                     f"Exception: {ex}")
+
+                LOGGER.info(f"Successfully sent price to the trading bots!")
+        except Exception as ex:
+            message = f"Error in MyClient.on_message(): {ex}"
+            raise Exception(message)
+
+
+def main():
+    try:
+
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        client = MyClient(intents=intents)
+        client.run(DISCORD_TOKEN)
+
+    except Exception as ex:
+        LOGGER.error(f"Error in main(): {ex}")
+        time.sleep(60)
+        main()
+
+
+main()
