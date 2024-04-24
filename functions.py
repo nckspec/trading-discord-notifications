7eb7b1ec549eb831feeeaf712267e4df5c975dd7
NSpector <nspectorit@gmail.com>
2024-04-24 00:40:46 -0700

    Made frontend toggle named bearish_put_spread that converts to bullish_bet

--- functions.py
+++ functions.py
@@ -206,9 +206,14 @@ def send_command_to_trading_bot(discord_username, command):
     #  Check if first word is 'set'
     #  if so, check if there are three words total, then perform an update
     if command[0] == "set" and len(command) == 3:
+
         #  Capitalize true/false so that the endpoint can insert it directly into field
         if command[2] == "true" or command[2] == "false":
             command[2] = command[2].capitalize()
+
+        if command[1] == "bearish_put_spread":
+            command[1] = "bearish_bet"
+
         success, response = update_user_account(discord_username, command[1], command[2])
 
 
