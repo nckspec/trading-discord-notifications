ad2eb5de7ad4cfb6641d9f1149ef06682d34785f
NSpector <nspectorit@gmail.com>
2024-04-29 19:55:23 -0700

    Added try statements around functions and separated a function

--- main.py
+++ main.py
@@ -87,6 +87,8 @@ class MyClient(discord.Client):
             if isinstance(message.channel, discord.DMChannel) and message.author != self.user:
                 #  Send the command to the trading bot
                 response = functions.send_command_to_trading_bot(str(message.author), message.content)
+
+                #  If the response is a dictionary, that means it contains an embed.
                 if isinstance(response, dict):
                     await message.author.send(content=response['content'], embed=response['embed'])
                 else:
